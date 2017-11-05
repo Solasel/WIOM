a9fdb52107f4cf350547667a5d9168d6e0d75218
William Walker <wewalker@berkeley.edu>
2017-11-04 18:46:35 -0700

    First draft of microcode generator complete.

--- utils/microcode/generator.c
+++ utils/microcode/generator.c
@@ -33,6 +33,7 @@ Write to the output file, with gaps of (INVALID) between valid instructions.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 struct string_pair {
 	char *instr_signal;
@@ -78,7 +79,7 @@ static char *err_msg(enum errs code)
 
 /* Function prototypes. */
 
-/* Useful error messages. */
+/* useful error messages. */
 static char *err_msg(enum errs code);
 
 /* Interprets the contents of src as microcode, and
@@ -90,12 +91,27 @@ static int read_src(char *src, char **fstring,
 		int *is_len, int *mc_len, char **inv,
 		int *num_x, struct string_pair **x_mc);
 
-/* Generates the sorted final list of microcode. */
-static int gen_fin_mc(struct string_pair *x_mc,
+/* Generates the final list of microcode. */
+static int gen_fin_mc(int num_x, struct string_pair *x_mc,
 		int *num_f, struct signal_pair **f_mc);
 
 /* Writes the final microcode to the destination file. */
-static int write_mc(char *dst, char *inv, int num_f, struct signal_pair *f_mc);
+static int write_mc(char *dst, int instr_sig_len,
+		char *inv, int num_f, struct signal_pair *f_mc);
+
+/* Counts the occurrences of c in str. */
+static int char_occ(const char *str, char c);
+
+/* Returns 2^x, when x is an integer. */
+static int two_pow(int x);
+
+/* Unpacks the 'x' containing string
+ * pairs into signal pairs in target. */
+static void unpack(char *instr_sig, char *mc,
+		int *index, struct signal_pair **target);
+
+/* Sorts a len long array of signal_pairs based on their instr_signal. */
+static int merge_sort_sig_pairs(int len, struct signal_pair *f_mc);
 
 /* Main. */
 int main(int argc, char *argv[])
@@ -164,9 +180,9 @@ static int generate_microcode(char *src, char *dst)
 
 	/* Using x_microcode, generates the following:
 	 * 	num_f		- number of final microcode entries.
-	 * 	f_microcode	- an array containing the final microcode,
-	 * 				sorted by instr_code.		   */
-	failure = gen_fin_mc(x_microcode,
+	 * 	f_microcode	- an unosrted array, containing the
+	 * 				final microcode.            */
+	failure = gen_fin_mc(num_x, x_microcode,
 			&num_f, &f_microcode);
 	free(x_microcode);
 	if (failure) {
@@ -175,12 +191,23 @@ static int generate_microcode(char *src, char *dst)
 		goto free_fstring;
 	}
 
-	/* Using invalid_mc, num_f, and f_microcode, writes microcode to dst. */
-	failure = write_mc(dst, invalid_mc, num_f, f_microcode);
+	/* Sorts the array of microcode. */
+	failure = merge_sort_sig_pairs(num_f, f_microcode);
+	if (failure) {
+		printf("%d: Failed to sort the array of microcode.\n",
+				__LINE__);
+		goto free_f_microcode;
+	}
+
+	/* Using instr_sig_len, invalid_mc, num_f, and
+	 * f_microcode, writes microcode to dst.       */
+	failure = write_mc(dst, instr_sig_len,
+			invalid_mc, num_f, f_microcode);
 	if (failure)
 		printf("%d: Failed to write microcode to '%s'.\n",
 				__LINE__, dst);
 
+free_f_microcode:
 	free(f_microcode);
 free_fstring:
 	free(fstring);
@@ -253,7 +280,7 @@ static int read_src(char *src, char **fstring,
 	*inv = line;
 
 	/* Rest of the file = microcode combinations. */
-	*x_mc = malloc((*num_x) * sizeof(struct string_pair));
+	*x_mc = malloc(*num_x * sizeof(struct string_pair));
 	if (!*x_mc) {
 		printf("%d: Failed to malloc a list of string_pairs for microcode in '%s'.\n",
 				__LINE__, src);
@@ -271,16 +298,168 @@ static int read_src(char *src, char **fstring,
 	return failure;
 }
 
-/* Generates the sorted final list of microcode. */
-static int gen_fin_mc(struct string_pair *x_mc,
+/* Generates the final list of microcode. */
+static int gen_fin_mc(int num_x, struct string_pair *x_mc,
 		int *num_f, struct signal_pair **f_mc)
 {
-	return NOT_YET_IMP;
+	int failure = 0;
+	int i, j;
+
+	/* Find the final number of microcode instructions. */
+	for (i = 0, *num_f = 0; i < num_x; i++) {
+		*num_f += two_pow(char_occ(x_mc[i].instr_signal, 'x'));
+	}
+
+	*f_mc = malloc(*num_f * sizeof(struct signal_pair));
+	if (!*f_mc) {
+		printf("%d: Failed to malloc a list of signal_pairs for microcode.\n",
+				__LINE__);
+		return MALLOC_FAILURE;
+	}
+
+	for (i = 0, j = 0; i < num_x; i++) {
+		unpack(x_mc[i].instr_signal, x_mc[i].microcode,
+				&j, f_mc);
+	}
+
+	return failure;
 }
 
 /* Writes the final microcode to the destination file. */
-static int write_mc(char *dst, char *inv, int num_f, struct signal_pair *f_mc)
+static int write_mc(char *dst, int instr_sig_len,
+		char *inv, int num_f, struct signal_pair *f_mc)
 {
-	return NOT_YET_IMP;
+	int failure = 0;
+	int max_addr = two_pow(instr_sig_len);
+	int i, addr_i;
+	long gap;
+	FILE *target;
+
+	target = fopen(dst, "w");
+	if (!target) {
+		printf("%d: Failed to open file '%s' for writing.\n",
+				__LINE__, dst);
+		return FOPEN_FAILURE;
+	}
+
+	fputs("v2.0 raw\n", target);
+	for (i = 0, addr_i = 0; i < num_f; i++) {
+		gap = f_mc[i].instr_code - addr_i;
+		if (gap) {
+			fprintf(target, "%ldx", gap);
+			fputs(inv, target);
+			fputc('\n', target);
+		}
+
+		fputs(f_mc[i].microcode, target);
+		fputc('\n', target);
+
+		addr_i = f_mc[i].instr_code + 1;
+	}
+
+	fclose(target);
+	return failure;
+}
+
+/* #################
+ * #### HELPERS ####
+ * ################# */
+
+/* Counts the occurrences of c in str. */
+static int char_occ(const char *str, char c)
+{
+	int i;
+	int count = 0;
+	for (i = 0; str[i]; i++)
+		count += str[i] == c ? 1: 0;
+	return count;
+}
+
+/* Returns 2^x, when x is an integer. */
+static int two_pow(int x)
+{
+	int rv = 1;
+	while (x--)
+		rv = 2 * rv;
+	return rv;
+}
+
+/* Unpacks the 'x' containing string
+ * pairs into signal pairs in target. */
+static void unpack(char *instr_sig, char *mc,
+		int *index, struct signal_pair **target)
+{
+	char *xptr;
+
+	/* If it's a final string, insert the signal pair. */
+	if (!(xptr = strchr(instr_sig, 'x'))) {
+		(*target)[*index].instr_code = strtol(instr_sig, NULL, 2);
+		(*target)[(*index)++].microcode = mc;
+		return;
+	}
+
+	*xptr = '0';
+	unpack(instr_sig, mc, index, target);
+	*xptr = '1';
+	unpack(instr_sig, mc, index, target);
+	*xptr = 'x';
+}
+
+/* Sorts a len long array of signal_pairs based on their instr_signal. */
+static int merge_sort_sig_pairs(int len, struct signal_pair *f_mc)
+{
+	int failure = 0;
+	int half = len / 2;
+	int i, low_it, upp_it;
+	struct signal_pair *lower_half, *upper_half;
+
+	/* If we have an array of length 1, we're done. */
+	if (len == 1)
+		return 0;
+
+	/* Otherwise, recursively sort the top/bottom... */
+	lower_half = malloc(len * sizeof(struct signal_pair));
+	if (!lower_half) {
+		printf("%d: Failed to malloc the lower_half array for merge-sorting.\n",
+				__LINE__);
+		return MALLOC_FAILURE;
+	}
+	upper_half = lower_half + half;
+
+	memcpy(lower_half, f_mc, len * sizeof(struct signal_pair));
+	failure = merge_sort_sig_pairs(half, lower_half);
+	if (failure) {
+		printf("%d: Failed to sort the lower_half array in merge-sorting.\n",
+				__LINE__);
+		goto free_copy;
+	}
+
+	failure = merge_sort_sig_pairs(len - half, upper_half);
+	if (failure) {
+		printf("%d: Failed to sort the upper_half array in merge-sorting.\n",
+				__LINE__);
+		goto free_copy;
+	}
+
+	/* And merge them together! */
+	for (i = 0, low_it = 0, upp_it = 0; i < len; i++) {
+		if (upp_it == len - half) {
+			f_mc[i] = lower_half[low_it];
+			low_it++;
+		} else if (low_it == half) {
+			f_mc[i] = upper_half[upp_it];
+			upp_it++;
+		} else if (lower_half[low_it].instr_code < upper_half[upp_it].instr_code) {
+			f_mc[i] = lower_half[low_it];
+			low_it++;
+		} else {
+			f_mc[i] = upper_half[upp_it];
+			upp_it++;
+		}
+	}
+
+free_copy:
+	free(lower_half);
+	return failure;
 }
 
